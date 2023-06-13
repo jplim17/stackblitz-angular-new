ef37a07b3e629639146fecf39136df6cc4f55df0
jplim17 <98366573+jplim17@users.noreply.github.com>
2023-06-13 20:35:22 +0800

    2 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  ngOnInit(): void {
-    console.log('asdas');
-  }
+  ngOnInit(): void {}
 }
